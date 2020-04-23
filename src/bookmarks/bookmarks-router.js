6fc84f7d1d25c4a6efbba79a5eaf29ae77d92a48
Claire Hendry <claire.d.hendry@gmail.com>
2020-04-23 11:44:24 -0400

    initial commit

---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const { lists } = require('../store')
+const { v4: uuid } = require('uuid')
+const logger = require('../logger')
+
+const bookmarksRouter = express.Router()
+const bodyParser = express.json()
+
+bookmarksRouter  
+    .route('/bookmarks')
+    .get((req, res) => {
+        res.send(lists)
+    })
+    .post(bodyParser, (req, res) => {
+        const { title, rating, description="" } = req.body;
+        if(!title) {
+            logger.error(`Title is required`);
+            return res.status(400).send('Invalid data');
+        }
+        if(!rating) {
+            logger.error(`Content is required`);
+            return res.status(400).send('Invalid data');
+        }
+        
+        const id = uuid();
+        const bookmark = {
+            id,
+            title,
+            rating,
+            description
+        };
+        lists.push(bookmark);
+             logger.info(`Bookmark with id ${id} created`);
+    
+        res 
+            .status(201)
+            .location(`http://localhost:8000/bookmarks/${id}`)
+            .json(bookmark)
+        })
+
+bookmarksRouter
+    .route('/bookmarks/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        const bookmark = lists.find(item => item.id == id);
+        if(!bookmark) {
+            logger.error(`Bookmark with id ${id} not found.`);
+            return res.status(404).send('Bookmark Not Found');
+        }
+        res.json(bookmark)
+        })
+    .delete((req, res) => {
+        const { id } = req.params;
+        const bookmarkIndex = lists.findIndex(item => item.id == id);
+        if (bookmarkIndex === -1) {
+            logger.error(`Bookmark with id ${id} not found.`);
+            return res
+                .status(404)
+                .send('Not found');
+        }
+        lists.splice(bookmarkIndex, 1);
+        logger.info(`Bookmark with id ${id} deleted.`);
+        res
+            .status(204)
+            .end();
+    })
+
+    module.exports = bookmarksRouter
---
+++ src/store.js
@@ -0,0 +1,17 @@
+const lists = [
+    {
+        title: "Bookmark 1",
+        rating: 9,
+        id: 1,
+        description: "The first bookmark"
+
+    },
+    {
+        title: "Bookmark 2",
+        rating: 8,
+        id: 2,
+        description: "The second bookmark"
+    }
+]
+
+module.exports = { lists }
